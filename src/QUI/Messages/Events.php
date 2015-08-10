36859352e96793e0efd8f15c5cff0c314b92528f
Henning Leutz <leutz@pcsg.de>
2015-08-10 08:27:35 +0200

    messages CSS Template Klassen

---
+++ src/QUI/Messages/Events.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file contains \QUI\Messages\Events
+ */
+namespace QUI\Messages;
+
+/**
+ * Event handling
+ *
+ * @author www.pcsg.de (Henning Leutz)
+ */
+class Events
+{
+    static function onTemplateGetHeader($Template)
+    {
+        $Template->extendHeaderWithCSSFile(URL_OPT_DIR .'quiqqer/messages/bin/messages.css');
+    }
+
+}
